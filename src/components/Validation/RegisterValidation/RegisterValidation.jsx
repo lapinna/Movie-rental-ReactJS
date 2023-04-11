28fbecb1edf97cae578f86337cb5dfd6ce74d4c8
Lapina <lapinna.inese@gmail.com>
2023-04-11 17:22:27 +0300

    update register validation

--- src/components/Validation/RegisterValidation/RegisterValidation.jsx
+++ src/components/Validation/RegisterValidation/RegisterValidation.jsx
@@ -1,6 +1,5 @@
 const RegisterValidation = (values) => {
   let errors = {};
-  console.log("values", values);
 
   const inputValues = Object.entries(values);
 
@@ -10,8 +9,12 @@ const RegisterValidation = (values) => {
       .filter((elem) => elem !== null)
       .map(isValidLength())
       .filter((elem) => elem !== null)
+      .map(isValidEmail())
+      .filter((elem) => elem !== null)
+      .map(isValidPassword())
+      .filter((elem) => elem !== null);
   }
-  
+
   function isValidContent() {
     return (elem) => {
       if (elem[1] === "") {
@@ -24,17 +27,43 @@ const RegisterValidation = (values) => {
 
   function isValidLength() {
     return (elem) => {
-      if ((elem[0] === "name" || elem[0] === "surname") &&
-        elem[1].length < 2) {
-        errors[elem[0]] = `${capitalizeFirst(
-          elem[0]
-        )} field should contain 2 or more letters`;
+      if ((elem[0] === "name" || elem[0] === "surname") && elem[1].length < 2) {
+        errors[elem[0]] = `${capitalizeFirst(elem[0])} field should contain 2 or more letters`;
         return null;
-      } else if ((elem[0] === "password1" || elem[0] === "password2") &&
-        elem[1].length < 8) {
-        errors[elem[0]] = `${capitalizeFirst(
-          elem[0]
-        )} field should contain 8 or more letters`;
+      }
+      return elem;
+    };
+  }
+
+  function isValidEmail() {
+    return (elem) => {
+      const email1 = values.email1;
+      const email2 = values.email2;
+      if (elem[0] === "email1" || elem[0] === "email2") {
+        if (!/\S+@\S+\.\S+/.test(elem[1])) {
+          errors[elem[0]] = `Email is not valid`;
+          return null;
+        }
+        if (email1 !== email2) {
+          console.log("here", email1 !== email2);
+          errors[elem[0]] = `Email field does not match`;
+          return null;
+        }
+      }
+      return elem;
+    };
+  }
+
+  function isValidPassword() {
+    return (elem) => {
+      const password1 = values.password1;
+      const password2 = values.password2;
+      if ((elem[0] === "password1" || elem[0] === "password2") && elem[1].length < 8) {
+        if (password1 !== password2) {
+          errors[elem[0]] = `Password field does not match`;
+          return null;
+        }
+        errors[elem[0]] = `${capitalizeFirst(elem[0])} field should contain 8 or more letters`;
         return null;
       }
       return elem;
